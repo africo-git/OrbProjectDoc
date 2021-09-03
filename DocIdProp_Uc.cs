ee7e86b707150e1881e9c5d2fa2a26920ce87198
africo-git <89541827+africo-git@users.noreply.github.com>
2021-09-03 14:31:22 +0200

    Añadido DocIdProp_Uc (vacío)

---
+++ DocIdProp_Uc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OrbProjectDoc
+{
+    public partial class DocIdProp_Uc : UserControl
+    {
+        public DocIdProp_Uc()
+        {
+            InitializeComponent();
+        }
+    }
+}
